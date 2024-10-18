55923eae6eaf1bcdf8c845e2968a8e3bae49d13c
xLune64 <dgamers.643@gmail.com>
2024-10-18 12:51:07 +0200

    Hechas correciones en UD2-07 y cambiado el archivo de debug

--- 02Estructuras_de_control/ola.py
+++ 02Estructuras_de_control/ola.py
@@ -1,12 +1,6 @@
-filas = int(input("cuantas filas quieres?"))
-columnas = 1
-contador = filas
-
-for i in range(1, filas+1):
-    for j in range(0, contador):
-        print(" ", end="")
-    for n in range (0, columnas):
-        print("*", end="")
-    columnas += 2
-    contador -= 1
-    print("\n", end="")
+bloques = int(input("cuantos bloques tienes?"))
+acumulador = 1
+while(bloques >= acumulador):
+    bloques -= acumulador
+    acumulador += 1
+print(acumulador-1)
